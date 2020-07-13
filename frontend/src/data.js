0696bfabc6e887e84074f13c25bb91b0c3a5a155
gangadhar733 <gangadharbiradar733@gmail.com>
2020-07-13 23:39:24 +0530

    Routing Homescreen and productscreen

---
+++ frontend/src/data.js
@@ -0,0 +1,30 @@
+export default {
+    products:[{
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:50,
+        brand:'Nike',
+        rating:4.2,
+        numReviews:5
+    },
+    {
+        name:'Best Shirt',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:8
+    }
+]
+}
